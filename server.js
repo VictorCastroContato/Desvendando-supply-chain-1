f66919155e1dbb0bc5b7fc77820d7f4049d35dfa
VictorCastroContato <contatovcastro98@gmail.com>
2026-06-17 15:53:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -79,6 +79,11 @@ function sanitizeContent(input) {
   }
   return out;
 } 
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.get('/admin-victor-supply', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'admin.html'));
